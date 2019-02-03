82d67ca9d5a8bd7a9d111cd57e4b015377153177
b1f6c1c4 <b1f6c1c4@gmail.com>
2019-02-03 16:11:05 -0500

    walker seems work

--- git-download.sh
+++ git-download.sh
@@ -170,7 +170,45 @@ fi
 
 LEGACY=YES # TODO
 
+fetcher()
+{
+    # $1 = worktree
+    # $2 = remote repo
+    # $3 = sha1
+
+    if [ ! -z "$VERBOSE" ]; then
+        echo "Fetcher:" "$@"
+    fi
+    F_REPO="$1/.git"
+    F_WORK="$1"
+    shift
+    F_REMO="$1"
+    shift
+    F_SHA1="$1"
+    shift
+
+    REMO_GIT=$(echo "$F_REMO" | sed -E 's_^https://([^/]+)/_git@\1:_')
+    if [ ! -z "$VERBOSE" ]; then
+        echo "Modified remote:" "$REMO_GIT"
+    fi
+
+    if [ ! -z "$LEGACY" ]; then
+        if [ ! -d "$F_REPO" ]; then
+            mkdir -p "$F_WORK"
+            git init "$F_WORK"
+        fi
+        git --git-dir="$F_REPO" fetch-pack --depth=1 "$REMO_GIT" "$F_SHA1"
+        if [ $? -ne 0 ]; then
+            exit 2
+        fi
+    else
+        echo "Unfortunately, this feature has not been implemented."
+        exit 233 # TODO
+    fi
+}
+
 WALKER_REPO=
+WALKER_WORK=
 WALKER_RESULT=
 WALKER_TYPE=
 WALKER_MODE=
@@ -178,8 +216,9 @@ walker()
 {
     # $1 = worktree
     # $2 = base sha1
-    # $3 = sha1
-    # $4 = target path
+    # $3 = path
+    # $4 = sha1
+    # $5 = target path
 
     # if [ -z "$2" ]; then
     #     TYPE=$(git --git-dir="$REPO_DIR" cat-file -t "$1")
@@ -193,14 +232,27 @@ walker()
     #     return
     # fi
 
-    W_REPO=$1.git
-    W_WORK=$1
-    W_BSHA=$2
-    W_SHA1=$3
-    W_TARG=$4
-    echo in walker "$1" "$2" "$3" "$4"
-    FIRST=$(echo "$W_TARG" | sed -E 's_^([^/]*)(/.*)?$_\1_')
-    REST=$(echo "$W_TARG" | sed -E 's_^[^/]*(/(.*))?$_\2_')
+    if [ ! -z "$VERBOSE" ]; then
+        echo "Walker:" "$@"
+    fi
+    W_REPO="$1/.git"
+    W_WORK="$1"
+    shift
+    W_BSHA="$1"
+    shift
+    W_PATH="$1"
+    shift
+    W_SHA1="$1"
+    shift
+    W_TARG="$1"
+    shift
+    FIRST=$(echo "$W_TARG" | sed -E 's_^([^/]*)(/.*)?$_\1_') # TODO: bad regex
+    REST=$(echo "$W_TARG" | sed -E 's_^[^/]*(/(.*))?$_\2_') # TODO: bad regex
+    if [ -z "$W_PATH" ]; then
+        WN_PATH="$FIRST"
+    else
+        WN_PATH="$W_PATH/$FIRST"
+    fi
     LSTREE="$(git --git-dir="$W_REPO" ls-tree "$W_SHA1" -- "$FIRST")"
     if [ $? -ne 0 ]; then
         exit 4
@@ -209,39 +261,61 @@ walker()
     TYPE=$2
     WN_SHA1=$3
     if [ "$TYPE" = "commit" ]; then
-        git --git-dir="$W_REPO" cat-file blob "$W_BSHA:.gitmodule" > "$WORK_DIR/walker_temp"
+        WN_WORK="$W_WORK/$WN_PATH"
+        git --git-dir="$W_REPO" cat-file blob "$W_BSHA:.gitmodules" > "$W_WORK/.gitmodules"
+        git config --file="$W_WORK/.gitmodules" --get-regexp 'submodule\..*\.path' > "$WORK_DIR/walker_temp"
         if [ $? -ne 0 ]; then
-            echo "Can't clone submodule: .gitmodule not found"
+            echo "Can't clone submodule: .gitmodules not found"
             exit 190
         fi
-        ID=$(grep -nF "$WN_SHA1" "$WORK_DIR/walker_temp" | cut -f1 -d:)
-        if [ -z "$ID" ]; then
-            echo "Can't clone submodule: submodule not found"
+        NM=$(grep -F ".path $WN_PATH" "$WORK_DIR/walker_temp")
+        if [ -z "$NM" ]; then
+            echo "Can't clone submodule: submodules not found"
             exit 191
         fi
-        URL=$(awk "FNR == $ID { print \$1; }" "$WORK_DIR/walker_temp")
-        if [ ! "$URL" = "URL" ]; then
-            echo "Can't clone submodule: url not found"
+        NMX=$(echo "$NM" | cut -f2 -d.)
+        URL=$(git config --file="$W_WORK/.gitmodules" --get "submodule.$NMX.url")
+        if [ ! -z "$VERBOSE" ]; then
+            echo "Submodule found:" "$NMX" "$URL"
+        fi
+        fetcher "$WN_WORK" "$URL" "$WN_SHA1"
+        WN_SHA1=$(git --git-dir="$WN_WORK/.git" rev-parse "$WN_SHA1^{tree}")
+        if [ $? -ne 0 ]; then
+            echo "Can't find tree from commit"
             exit 192
         fi
-        URL=$(awk "FNR == $ID { print \$3; }" "$WORK_DIR/walker_temp")
-        # TODO
-    elif [ "$TYPE" = "tree" ]; then
-        if [ -z $REST ]; then
-            WALKER_REPO="$W_REPO"
+        if [ -z "$REST" ]; then
+            WALKER_WORK="$WN_WORK"
             WALKER_RESULT="$WN_SHA1"
             WALKER_TYPE="tree"
             WALKER_MODE=
-            # TODO
+            if [ ! -z "$VERBOSE" ]; then
+                echo "Walker suceed:" "$WALKER_WORK" "$WALKER_RESULT" "$WALKER_TYPE" "$WALKER_MODE"
+            fi
         else
-            walker "$W_WORK" "$W_BSHA" "$WN_SHA1" "$REST"
+            walker "$WN_WORK" "$WN_SHA1" "." "$WN_SHA1" "$REST"
+        fi
+    elif [ "$TYPE" = "tree" ]; then
+        if [ -z "$REST" ]; then
+            WALKER_WORK="$W_WORK"
+            WALKER_RESULT="$WN_SHA1"
+            WALKER_TYPE="tree"
+            WALKER_MODE=
+            if [ ! -z "$VERBOSE" ]; then
+                echo "Walker suceed:" "$WALKER_WORK" "$WALKER_RESULT" "$WALKER_TYPE" "$WALKER_MODE"
+            fi
+        else
+            walker "$W_WORK" "$W_BSHA" "$WN_PATH" "$WN_SHA1" "$REST"
         fi
     elif [ "$TYPE" = "blob" ]; then
-        if [ -z $REST ]; then
-            WALKER_REPO="$W_REPO"
+        if [ -z "$REST" ]; then
+            WALKER_WORK="$W_WORK"
             WALKER_RESULT="$WN_SHA1"
             WALKER_TYPE="blob"
             WALKER_MODE="$1"
+            if [ ! -z "$VERBOSE" ]; then
+                echo "Walker suceed:" "$WALKER_WORK" "$WALKER_RESULT" "$WALKER_TYPE" "$WALKER_MODE"
+            fi
         else
             echo "$W_SHA1/$FIRST is a file, you can't walk into it"
             exit 17
@@ -250,59 +324,75 @@ walker()
         echo "Type $TYPE not supported"
         exit 20
     fi
+    WALKER_REPO="$WALKER_WORK/.git"
+}
+
+recurse()
+{
+    if [ ! -z "$VERBOSE" ]; then
+        echo "Recurse:" "$@"
+    fi
+}
+
+exporter()
+{
+    # $1: repo
+    # $2: work
+    # $3: sha1
+    # $4: target
+
+    if [ ! -z "$VERBOSE" ]; then
+        echo "Exporter:" "$@"
+    fi
+    E_REPO="$1"
+    shift
+    E_WORK="$1"
+    shift
+    E_RESULT="$1"
+    shift
+    E_OUTPUT="$1"
+    shift
+
+    git --git-dir="$E_REPO" --work-tree="$E_WORK" read-tree "$E_RESULT"
+    if [ -d "$OUTPUT" ]; then
+        if [ ! -z "$FORCE_DIR" ]; then
+            rm -rf "$E_OUTPUT"
+        else
+            echo "Output path is a folder and you try to replace it with a folder; use --rm-rf if that's what you want."
+            exit 3
+        fi
+    elif [ -f "$OUTPUT" ]; then
+        if [ ! -z "$FORCE" ]; then
+            rm -f "$E_OUTPUT"
+        else
+            echo "Output path is a file and you try to replace it with a folder; use --force if that's what you want."
+            exit 3
+        fi
+    fi
+    git --git-dir="$E_REPO" --work-tree="$E_WORK" checkout-index -f --prefix="result/" -a
+    if [ $? -ne 0 ]; then
+        exit 4
+    fi
+    mv "$E_WORK/result" "$E_OUTPUT"
+    if [ $? -ne 0 ]; then
+        exit 6
+    fi
+    if [ ! -z "$RECURSIVE" ]; then
+        git --git=dir="$E_REPO" ls-tree "$E_RESULT" | grep '^160000 commit ' | xargs -n 1 recurse "$E_WORK" "$E_OUTPUT"
+    fi
 }
 
 if [ ! -z "$LEGACY" ]; then
-    git --git-dir="$REPO_DIR" fetch-pack --depth=1 "$REPO" "$SHA1"
-    if [ $? -ne 0 ]; then
-        exit 2
-    fi
+    fetcher "$WORK_DIR" "$REPO" "$SHA1"
+    walker "$WORK_DIR" "$SHA1" "" "$SHA1" "$DIR"
 
-    echo "walker: $(walker "$SHA1" "." "$DIR")"
-
-    if [ -z "$DIR" ]; then
-        TYPE=tree
-    else
-        TYPE=$(git --git-dir="$REPO_DIR" cat-file -t "$SHA1:$DIR")
-        if [ $? -ne 0 ]; then
-            exit 4
-        fi
-    fi
     if [ ! -z "$VERBOSE" ]; then
-        echo "Type:" $TYPE
+        echo "Type:" "$WALKER_TYPE"
     fi
 
-    git --git-dir="$REPO_DIR" --work-tree="$WORK_DIR" read-tree "$SHA1"
-
-    if [ "$TYPE" = "tree" ]; then
-        if [ -d "$OUTPUT" ]; then
-            if [ ! -z "$FORCE_DIR" ]; then
-                rm -rf "$OUTPUT"
-            else
-                echo "Output path is a folder and you try to replace it with a folder; use --rm-rf if that's what you want."
-                exit 3
-            fi
-        elif [ -f "$OUTPUT" ]; then
-            if [ ! -z "$FORCE" ]; then
-                rm -f "$OUTPUT"
-            else
-                echo "Output path is a file and you try to replace it with a folder; use --force if that's what you want."
-                exit 3
-            fi
-        fi
-        if [ -z "$DIR" ]; then
-            git --git-dir="$REPO_DIR" --work-tree="$WORK_DIR" checkout-index -f --prefix="result/" -- "$DIR"
-        else
-            git --git-dir="$REPO_DIR" --work-tree="$WORK_DIR" checkout-index -f --prefix="result/" -a
-        fi
-        if [ $? -ne 0 ]; then
-            exit 4
-        fi
-        mv "$WORK_DIR/result/$DIR" "$OUTPUT"
-        if [ $? -ne 0 ]; then
-            exit 6
-        fi
-    elif [ "$TYPE" = "blob" ]; then
+    if [ "$WALKER_TYPE" = "tree" ]; then
+        exporter "$WALKER_REPO" "$WALKER_WORK" "$WALKER_RESULT" "$OUTPUT"
+    elif [ "$WALKER_TYPE" = "blob" ]; then
         if [ -d "$OUTPUT" ]; then
             if [ ! -z "$FORCE_DIR" ]; then
                 rm -rf "$OUTPUT"
@@ -318,16 +408,23 @@ if [ ! -z "$LEGACY" ]; then
                 exit 3
             fi
         fi
-        git --git-dir="$REPO_DIR" --work-tree="$WORK_DIR" checkout-index -f --prefix="result/" -- "$DIR"
+        git --git-dir="$WALKER_REPO" cat-file blob "$WALKER_RESULT" > "$WALKER_WORK/result"
         if [ $? -ne 0 ]; then
             exit 4
         fi
-        mv "$WORK_DIR/result/$DIR" "$OUTPUT"
+        if [ "$WALKER_MODE" = "100644" ]; then
+            chmod 644 "$WALKER_WORK/result"
+        elif [ "$WALKER_MODE" = "100755" ]; then
+            chmod 755 "$WALKER_WORK/result"
+        else
+            echo "Warning: mode not supported: $WALKER_MODE"
+        fi
         if [ $? -ne 0 ]; then
             exit 4
         fi
-    elif [ "$TYPE" = "commit" ]; then
-        echo "TODO: recursive"
-        exit 255
+        mv "$WALKER_WORK/result" "$OUTPUT"
+        if [ $? -ne 0 ]; then
+            exit 4
+        fi
     fi
 fi
