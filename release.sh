1a639e485864c0f10de2f7f28221ea8646c5f998
b1f6c1c4 <b1f6c1c4@gmail.com>
2022-07-21 19:47:57 -0400

    [ci skip] zsh-completion

--- release.sh
+++ release.sh
@@ -4,15 +4,23 @@ set -eux
 
 find build/ -mindepth 1 -delete
 
-mkdir -p build/bin/ build/share/man/man1/ build/share/licenses/git-get/ build/arch/
 VERSION="$(git describe --always --dirty | sed 's/-/_/g')"
 DATE="$(date -Id)"
 
-cp LICENSE build/share/licenses/git-get/
+mkdir -p build/bin/
 sed "s|GIT_GET_VERSION=|GIT_GET_VERSION=$VERSION|" git-get >build/bin/git-get
 sed "s|GIT_GET_VERSION=|GIT_GET_VERSION=$VERSION|" git-gets >build/bin/git-gets
+
+mkdir -p build/share/man/man1/
 sed "s|GIT_GET_VERSION|$VERSION|; s|GIT_GET_DATE|$DATE|" man/git-get.1 >build/share/man/man1/git-get.1
 sed "s|GIT_GET_VERSION|$VERSION|; s|GIT_GET_DATE|$DATE|" man/git-gets.1 >build/share/man/man1/git-gets.1
+
+mkdir -p build/share/licenses/git-get/
+cp LICENSE build/share/licenses/git-get/
+
+mkdir -p build/share/zsh/site-functions/
+cp zsh/_git-get build/share/zsh/site-functions/_git-get
+cp zsh/_git-gets build/share/zsh/site-functions/_git-gets
 
 chmod +x build/bin/git-get build/bin/git-gets
 
@@ -29,6 +37,7 @@ gpg --detach-sign build/git-get.tar.gz
 gpg --detach-sign build/git-get.tar.xz
 gpg --detach-sign build/git-get.zip
 
+mkdir -p build/arch/
 cat - <<EOF >build/arch/PKGBUILD
 # Maintainer: b1f6c1c4 <b1f6c1c4@gmail.com>
 pkgname=git-get
